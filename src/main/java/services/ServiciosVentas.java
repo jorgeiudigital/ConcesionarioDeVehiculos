b6eea4a8c870c3931c9489f2915f160283ce09aa
Jorge Andres Echavarria Pardo <jorgean0827@gmail.com>
2025-05-09 20:21:37 -0500

    Creación del servicio de venta para registrar una venta y mostrarla

--- src/main/java/services/ServiciosVentas.java
+++ src/main/java/services/ServiciosVentas.java
@@ -1,4 +1,31 @@
 package services;
 
+import models.Vehiculo;
+import models.Venta;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiciosVentas {
+
+    List<Venta> ventas = new ArrayList<>();
+
+    public void registrarVenta(double montoventa, Vehiculo vehiculo, String apellidoCliente, String nombreCliente, String documentoCliente) {
+        Venta venta = new Venta(montoventa, vehiculo, apellidoCliente, nombreCliente, documentoCliente);
+        ventas.add(venta);
+    }
+
+    public void mostrarVentas() {
+
+        if (ventas.isEmpty()) {
+            System.out.println("No hay ventas registradas");
+        } else {
+            System.out.println("Las ventas registradas son: ");
+            for (Venta venta : ventas) {
+                System.out.println(venta);
+            }
+
+        }
+
+    }
 }
